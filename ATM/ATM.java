7438f0d7a078d9e213af3f9ef9c691c90e81050c
engineve <yeindap@gmail.com>
2020-08-31 20:40:25 +0900

    UPDATE ATM
    bank name added

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,12 +1,15 @@
 public class ATM {
     public String bank;
     private static double money; // if other class can't access so use set_money method to change this data
-
+    
+    public void set_bank(String name){
+        bank=name;
+    }
     public void set_money(double amount){
         money=amount;
     }
     public void display(){
-        System.out.println("Money deposit : "+money); // Money deposit : 5000.0
+        System.out.printf("Money deposit in %s : %.1f",bank, money); // Money deposit : 5000.0
     }
     public static void main(String[] args){
     
--- ATM/user.java
+++ ATM/user.java
@@ -2,8 +2,9 @@ public class user {
 
     public static void main(String[] args){
         ATM user1=new ATM();
+        user1.set_bank("ABC");
         user1.set_money(5000);
-        user1.display(); // Money deposit : 5000.0
+        user1.display(); // Money deposit in ABC : 5000.0
     }
     
 }
