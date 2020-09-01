7d12615e577cacf155a1ed56aeba15a06c8545e3
engineve <yeindap@gmail.com>
2020-09-01 17:08:20 +0900

    ADD streamEX
    FileOutputStream, FileInputStream

---
+++ streamEx/input.java
@@ -0,0 +1,29 @@
+// Package java.lang;
+// public class System{
+//    public static PrintStream out; // System.out
+//    public static InputStream in; // System.in
+//    public static PrintStream err; // System.err
+// }
+
+import java.io.*;
+
+public class input{
+    
+    public static void main(String[] args) throws IOException{
+        char c=(char) System.in.read();
+        System.out.println("1 : "+c);
+
+        FileOutputStream data=new FileOutputStream("data.txt");
+        data.write(1); // void write(byte[] b) byte 배열 형식 저장
+        data.write(2);  // void write(int b) byte 형식 저장
+        data.write(3);
+        data.close();
+
+        FileInputStream item=new FileInputStream("data.txt");
+        int i;
+        while((i=item.read())!=-1){
+            System.out.print(i);
+        }
+        item.close();
+    }
+}
