55fd77283fc17092712fbcc4fa59cd564250e7f1
engineve <yeindap@gmail.com>
2020-09-14 15:50:29 +0900

    UPDATE arrayBasic.java
    Scanner input

--- arrayBasic/arrayBasic.java
+++ arrayBasic/arrayBasic.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class arrayBasic {
     static int[] arr=new int[] {1,2,3,4,5};
     static int[] arr1={6,7,8,9,10};
 
     public static void main(String[] args){
         for(int i=0 ; i<arr.length; i++)
-            System.out.println(arr[i]); // 1 2 3 4 5
+        System.out.println(arr[i]); // 1 2 3 4 5
+
+        int[] data=new int[3];
+        Scanner s=new Scanner(System.in);
+        String item;
+        for(int j=0 ; j<data.length ; j++){
+            item=s.next();
+            data[j]=Integer.parseInt(item);
+        }
+        System.out.printf(data[0]+" "+data[1]+" "+data[2]);
+       
     }
     
 }
