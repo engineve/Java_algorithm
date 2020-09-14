9466ecae6534dd80699c76b256676d52949f6672
engineve <yeindap@gmail.com>
2020-09-14 15:42:59 +0900

    CREATE arrayBasic.java
    int array

---
+++ arrayBasic/arrayBasic.java
@@ -0,0 +1,10 @@
+public class arrayBasic {
+    static int[] arr=new int[] {1,2,3,4,5};
+    static int[] arr1={6,7,8,9,10};
+
+    public static void main(String[] args){
+        for(int i=0 ; i<arr.length; i++)
+            System.out.println(arr[i]); // 1 2 3 4 5
+    }
+    
+}
